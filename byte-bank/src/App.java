aa5ccff56926ea4d206c249756424172f0f8189a
Guilherme Beidaki Peres <guilherme.peres@matera.com>
2020-08-11 11:57:42 -0300

    finalizado curso introdução OO

--- byte-bank/src/App.java
+++ byte-bank/src/App.java
@@ -1,24 +1,41 @@
 public class App {
     public static void main(String[] args) {
-        Conta minhaConta = new Conta();
-        Conta contaDaAna = new Conta();
+        Cliente guilherme = new Cliente();
+        guilherme.setNome("Guilherme Beidaki");
+        guilherme.setCpf("222.222.222-22");
+        guilherme.setProfissao("Programador");
+
+        Cliente ana = new Cliente();
+        ana.setNome("Ana Luisa");
+        ana.setCpf("222.222.222-22");
+        ana.setProfissao("Médica");
+
+        Conta minhaConta = new Conta(1, 2);
+        minhaConta.setTitular(guilherme);
+
+        Conta contaDaAna = new Conta(1, 2);
+        contaDaAna.setTitular(ana);
 
         minhaConta.depositar(1000);
-        System.out.println(minhaConta.saldo);
-        System.out.println(contaDaAna.saldo);
+        System.out.println(minhaConta.getSaldo());
+        System.out.println(contaDaAna.getSaldo());
 
         if (minhaConta.transferir(contaDaAna, 500)){
             System.out.println("Consegui transferir");
         }
 
-        System.out.println(minhaConta.saldo);
-        System.out.println(contaDaAna.saldo);
+        System.out.println(minhaConta.getSaldo());
+        System.out.println(contaDaAna.getSaldo());
 
         if (minhaConta.transferir(contaDaAna, 2000)){
             System.out.println("Consegui transferir");
         }
 
-        System.out.println(minhaConta.saldo);
-        System.out.println(contaDaAna.saldo);
+        System.out.println(minhaConta.getSaldo());
+        System.out.println(contaDaAna.getSaldo());
+
+        System.out.println(minhaConta.getTitular().getNome());
+        System.out.println(contaDaAna.getTitular().getNome());
+        System.out.println("O total de contas é " + Conta.getTotal());
     }
 }
--- byte-bank/src/Conta.java
+++ byte-bank/src/Conta.java
@@ -1,39 +1,71 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
+    private static int total;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public Conta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+
+        Conta.total++;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
+    }
 
     private boolean podeRetirar(double valor) {
-        return saldo - valor >= 0;
+        return this.saldo - valor >= 0;
     }
 
     private void retirar(double valor) {
-        saldo -= valor;
+        this.saldo -= valor;
     }
 
-    void depositar(double valor) {
-        saldo += valor;
+    public void depositar(double valor) {
+        this.saldo += valor;
     }
 
-    boolean sacar(double valor) {
-        boolean podeSacar = podeRetirar(valor);
+    public boolean sacar(double valor) {
+        boolean podeSacar = this.podeRetirar(valor);
 
         if (podeSacar) {
-            retirar(valor);
+            this.retirar(valor);
         }
 
         return podeSacar;
     }
 
-    boolean transferir(Conta conta, double valor) {
-        boolean podeTransferir = podeRetirar(valor);
+    public boolean transferir(Conta conta, double valor) {
+        boolean podeTransferir = this.podeRetirar(valor);
 
         if (podeTransferir) {
-            retirar(valor);
+            this.retirar(valor);
             conta.depositar(valor);
         }
 
         return podeTransferir;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
     }
 }
