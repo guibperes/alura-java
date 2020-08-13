bb4af0eee507cbc36b3b57ec3a80f013496b1ad4
Guilherme Beidaki Peres <guibperes@protonmail.com>
2020-08-12 21:20:16 -0300

    heranca funcionario

--- byte-bank-herancas-interfaces/src/App.java
+++ byte-bank-herancas-interfaces/src/App.java
@@ -1,17 +1,25 @@
 public class App {
   public static void main(String[] args) {
     Funcionario funcionario =  new Funcionario();
-
-    funcionario.setNome("Guilherme Beidaki Peres");
-    funcionario.setCpf("222.222.222-22");
     funcionario.setSalario(1000);
 
     System.out.println(funcionario.getBonificacao());
 
     Gerente gerente = new Gerente();
-
     gerente.setSalario(2000);
 
     System.out.println(gerente.getBonificacao());
+
+    EditorVideo editorVideo =  new EditorVideo();
+    editorVideo.setSalario(1500);
+
+    System.out.println(editorVideo.getBonificacao());
+
+    ControleBonificacao controleBonificacao = new ControleBonificacao();
+    controleBonificacao.registrarFuncionario(funcionario);
+    controleBonificacao.registrarFuncionario(gerente);
+    controleBonificacao.registrarFuncionario(editorVideo);
+
+    System.out.println(controleBonificacao.getTotal());
   }
 }
